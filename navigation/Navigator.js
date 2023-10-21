fbc48fdc2d97b3cf36eec389c5007e34ea593823
IvanJuricic <ivanjuricic97@gmail.com>
2023-10-21 23:34:39 +0200

    Add initial screens and navigation

---
+++ navigation/Navigator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+import SplashScreen from '../screens/SplashScreen';
+
+const Stack = createStackNavigator();
+
+const CustomNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default CustomNavigator;
